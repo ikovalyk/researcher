8ab4d2d147c1a2d71427687186e5a851b6f776b0
Igor Kovalyk <xlihor@gmail.com>
2014-03-05 02:14:15 +0200

    SQL queries:  - create table employee  - fix bug with sequence

---
+++ src/main/resources/db/create_table_employee.sql
@@ -0,0 +1,13 @@
+﻿BEGIN TRANSACTION;
+
+CREATE TABLE EMPLOYEES
+  (
+     id        INT PRIMARY KEY,
+     firstname VARCHAR(30),
+     lastname  VARCHAR(30),
+     telephone VARCHAR(15),
+     email     VARCHAR(30),
+     created   TIMESTAMP DEFAULT NOW()
+  );
+
+ROLLBACK;
